33d4aa29bff83ca52fb1a728e6a307d4b9fe52e4
Tariqul Islam Parbat <2023200000635@seu.edu.bd>
2026-05-24 06:50:54 +0600

    application

---
+++ NrCarCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.nrcarcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrCarCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NrCarCenterApplication.class, args);
+    }
+
+}
